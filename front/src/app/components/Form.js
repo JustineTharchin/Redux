8d4d4ceefec73a1538d6884b02e70b6e6273334a
Justine <justine.tharchin@acensi.fr>
2023-11-03 10:40:58 +0100

    exo 4 + reorganisation

--- front/src/app/components/Form.js
+++ front/src/app/components/Form.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TextField, Select, MenuItem, Button, FormControl, InputLabel } from '@mui/material';
 
 function Form() {
+  const [formData, setFormData] = useState({ email: '', password: '', role: 'user' });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
   return (
-    <form>
-      <TextField label="Email" variant="outlined" fullWidth />
-      <TextField label="Password" variant="outlined" fullWidth />
+    <form onSubmit={handleSubmit}>
+      <TextField label="Email" variant="outlined" value={formData.email} onChange={handleInputChange} fullWidth />
+      <TextField label="Password" variant="outlined" value={formData.password} onChange={handleInputChange} fullWidth />
       <FormControl variant="outlined" fullWidth>
         <InputLabel>Role</InputLabel>
-        <Select label="Role">
+        <Select label="Role" name="role" value={formData.role} onChange={handleInputChange}>
           <MenuItem value="user">Utilisateur</MenuItem>
           <MenuItem value="admin">Administrateur</MenuItem>
         </Select>
